6ef5eb861808e7a3fa589273bd8ee3e9ecee6dbe
AlIDJAT <alidjatou1@gmail.com>
2025-03-09 19:42:23 -0400

    TDD - Step 2: Implemented User entity with essential fields"

---
+++ src/main/java/com/akkorhotel/domain/entity/User.java
@@ -0,0 +1,33 @@
+package com.akkorhotel.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String pseudo;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
+
--- src/test/java/com/akkorhotel/domaine/entity/UserTest.java
+++ src/test/java/com/akkorhotel/domaine/entity/UserTest.java
@@ -1,5 +1,6 @@
 package com.akkorhotel.domaine.entity;
 
+import com.akkorhotel.domain.entity.User;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
