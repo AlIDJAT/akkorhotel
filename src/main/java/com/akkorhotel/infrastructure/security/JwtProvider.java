c5368879169bc666e251052e433dcb210977def4
AlIDJAT <alidjatou1@gmail.com>
2025-03-10 17:35:50 +0100

    TDD - Step 22: Implemented JwtProvider (GREEN phase)"

---
+++ src/main/java/com/akkorhotel/infrastructure/security/JwtProvider.java
@@ -0,0 +1,22 @@
+package com.akkorhotel.infrastructure.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 jour
+                .signWith(key)
+                .compact();
+    }
+}
