f2c4bb9c104523f9d1ca80f30f99689563036fa5
AlIDJAT <alidjatou1@gmail.com>
2025-03-10 12:25:15 +0100

    TDD - Step 10: Implemented UserController to pass test (GREEN phase)

---
+++ src/main/java/com/akkorhotel/interface_adapters/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.akkorhotel.interface_adapters.controller;
+
+import com.akkorhotel.domain.entity.User;
+import com.akkorhotel.domain.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+}
