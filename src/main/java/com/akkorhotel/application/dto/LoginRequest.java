ac8f9f4a6298e916b4cdb3684a68920415e815b3
AlIDJAT <alidjatou1@gmail.com>
2025-03-10 16:26:01 +0100

    TDD - Step 15: Created LoginRequest and LoginResponse (RED phase continues)

---
+++ src/main/java/com/akkorhotel/application/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.akkorhotel.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/akkorhotel/application/dto/LoginResponse.java
@@ -0,0 +1,10 @@
+package com.akkorhotel.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+}
