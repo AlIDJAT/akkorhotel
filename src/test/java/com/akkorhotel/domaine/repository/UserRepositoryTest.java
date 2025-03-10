df4e607f3b724eac050f0e7b7eec5c454f99a6f7
AlIDJAT <alidjatou1@gmail.com>
2025-03-09 20:17:33 -0400

    TDD - Step 4: Implemented UserRepository to pass test using h2database (GREEN phase)

--- src/test/java/com/akkorhotel/domaine/repository/UserRepositoryTest.java
+++ src/test/java/com/akkorhotel/domaine/repository/UserRepositoryTest.java
@@ -2,9 +2,13 @@ package com.akkorhotel.domaine.repository;
 
 import com.akkorhotel.domain.entity.User;
 import com.akkorhotel.domain.entity.UserRole;
+import com.akkorhotel.domain.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -23,9 +27,9 @@ class UserRepositoryTest {
                 .build();
 
         userRepository.save(user);
-        User foundUser = userRepository.findByEmail("john.doe@example.com");
+        Optional<User> foundUser = userRepository.findByEmail("john.doe@example.com");
 
         assertThat(foundUser).isNotNull();
-        assertThat(foundUser.getEmail()).isEqualTo("john.doe@example.com");
+        assertThat(foundUser.get().getEmail()).isEqualTo("john.doe@example.com");
     }
 }
