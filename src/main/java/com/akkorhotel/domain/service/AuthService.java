bda3e1a3b8c57d185ebd3bf284701d554cb06a7d
AlIDJAT <alidjatou1@gmail.com>
2025-03-10 16:40:37 +0100

    TDD - Step 19: Refactored AuthService to query database (REFACTOR phase)

--- src/main/java/com/akkorhotel/domain/service/AuthService.java
+++ src/main/java/com/akkorhotel/domain/service/AuthService.java
@@ -2,14 +2,29 @@ package com.akkorhotel.domain.service;
 
 import com.akkorhotel.application.dto.LoginRequest;
 import com.akkorhotel.application.dto.LoginResponse;
+import com.akkorhotel.domain.entity.User;
+import com.akkorhotel.domain.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthService {
+    private final UserRepository userRepository;
+
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public LoginResponse authenticate(LoginRequest request) {
-        if (!"john.doe@example.com".equals(request.getEmail())) {
+        Optional<User> user = userRepository.findByEmail(request.getEmail());
+
+        if (user.isEmpty()) {
             throw new IllegalArgumentException("Invalid credentials");
         }
-        return new LoginResponse("mocked-token");
+
+        String token = "jwtProvider.generateToken(user.get().getEmail());";
+        return new LoginResponse(token);
     }
 }
