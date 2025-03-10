bc5968650b12e2e99ee0ad34346fd3e64d2babb7
AlIDJAT <alidjatou1@gmail.com>
2025-03-10 12:31:22 +0100

    TDD - Step 12: factored UserController with error handling (GREEN phase - REFACTOR)

--- src/main/java/com/akkorhotel/interface_adapters/controller/UserController.java
+++ src/main/java/com/akkorhotel/interface_adapters/controller/UserController.java
@@ -16,8 +16,12 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        User createdUser = userService.createUser(user);
-        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        try {
+            User createdUser = userService.createUser(user);
+            return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
