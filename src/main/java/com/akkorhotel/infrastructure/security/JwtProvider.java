5adb5f76a366bb5f3fc3238a703f1c515e8e1294
AlIDJAT <alidjatou1@gmail.com>
2025-03-11 01:28:42 +0100

    Step 43 GREEN: Implement JWT validation method"

--- src/main/java/com/akkorhotel/infrastructure/security/JwtProvider.java
+++ src/main/java/com/akkorhotel/infrastructure/security/JwtProvider.java
@@ -19,4 +19,14 @@ public class JwtProvider {
                 .signWith(key)
                 .compact();
     }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
