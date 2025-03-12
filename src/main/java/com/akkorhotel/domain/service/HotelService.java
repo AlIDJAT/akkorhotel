c7513946bd0c4ae791256bcfa39bc2b823d056e0
AlIDJAT <alidjatou1@gmail.com>
2025-03-12 15:33:25 +0100

    Step 54 GREEN: Implement getHotelById method

--- src/main/java/com/akkorhotel/domain/service/HotelService.java
+++ src/main/java/com/akkorhotel/domain/service/HotelService.java
@@ -2,7 +2,10 @@ package com.akkorhotel.domain.service;
 
 import com.akkorhotel.domain.entity.Hotel;
 import com.akkorhotel.domain.repository.HotelRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HotelService {
 
     private final HotelRepository hotelRepository;
@@ -13,5 +16,10 @@ public class HotelService {
 
     public Hotel createHotel(Hotel hotel) {
         return hotelRepository.save(hotel);
+    }
+
+    public Hotel getHotelById(Long id) {
+        return hotelRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Hotel not found"));
     }
 }
