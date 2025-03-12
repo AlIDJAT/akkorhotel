a9b6b3c3bf21151e797eaabdd327c7f8861692c5
AlIDJAT <alidjatou1@gmail.com>
2025-03-12 15:47:40 +0100

    Step 57 GREEN: Implement getHotelById endpoint in HotelController

---
+++ src/main/java/com/akkorhotel/interface_adapters/controller/HotelController.java
@@ -0,0 +1,31 @@
+package com.akkorhotel.interface_adapters.controller;
+
+import com.akkorhotel.domain.entity.Hotel;
+import com.akkorhotel.domain.exception.HotelNotFoundException;
+import com.akkorhotel.domain.service.HotelService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/hotels")
+public class HotelController {
+
+    private final HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Hotel> getHotelById(@PathVariable Long id) {
+        return ResponseEntity.ok(hotelService.getHotelById(id));
+    }
+
+    @ExceptionHandler(HotelNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleHotelNotFound(HotelNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", ex.getMessage()));
+    }
+}
